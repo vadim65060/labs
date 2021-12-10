fd8109bc58566384d98ffcee295919f664b1d41b
vadim65060 <vadim65060@users.noreply.github.com>
2021-12-10 16:40:17 +0300

    add dynamic array

--- Lab12.3/main.c
+++ Lab12.3/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "ArrrayProcessing.h"
 
 int main() {
     int n;
     printf("12.3)n=");
     scanf("%i", &n);
-    int a[n];
+    int *a = (int *) malloc(n * sizeof(int));
     for (size_t i = 0; i < n; ++i) {
         scanf("%i", &a[i]);
     }
-    n = (int)(DeleteEvenNumbers(a, n));
+    n = (int) (DeleteEvenNumbers(a, n));
     for (size_t i = 0; i < n; ++i) {
         printf("%i ", a[i]);
     }
+    free(a);
 }
