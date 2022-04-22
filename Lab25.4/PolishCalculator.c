5e72a649e3a237088a5c5d03d488af7c900356d2
vadim65060 <vadim65060@users.noreply.github.com>
2022-04-22 15:09:26 +0300

    complete

--- Lab25.4/PolishCalculator.c
+++ Lab25.4/PolishCalculator.c
@@ -8,37 +8,14 @@
 
 
 int Calculate(Stack *stack, char *operation) {
-    if (stack->len < 1)return -1;
-    if (strcmp(operation, "+") == 0) {
-        if (stack->len < 2)
-            return -1;
-        double n1 = Pop(stack);
-        double n2 = Pop(stack);
-        Push(stack, n1 + n2);
-    } else if (strcmp(operation, "-") == 0) {
-        if (stack->len < 2)
-            return -1;
-        double n1 = Pop(stack);
-        double n2 = Pop(stack);
-        Push(stack, n2 - n1);
-    } else if (strcmp(operation, "*") == 0) {
-        if (stack->len < 2)
-            return -1;
-        double n1 = Pop(stack);
-        double n2 = Pop(stack);
-        Push(stack, n2 * n1);
-    } else if (strcmp(operation, "/") == 0) {
-        if (stack->len < 2)
-            return -1;
-        double n1 = Pop(stack);
-        double n2 = Pop(stack);
-        Push(stack, n2 / n1);
-    } else if (strcmp(operation, "^") == 0) {
-        if (stack->len < 2)
-            return -1;
-        double n1 = Pop(stack);
-        double n2 = Pop(stack);
-        Push(stack, pow(n2, n1));
+    if (strcmp(operation, "e") == 0) {
+        Push(stack, exp(1));
+    } else if (strcmp(operation, "print") == 0) {
+        return 0;
+    } else if (stack->len < 1)
+        return -1;
+    else if (strcmp(operation, "del") == 0) {
+        Pop(stack);
     } else if (strcmp(operation, "inv") == 0) {
         Push(stack, 1 / Pop(stack));
     } else if (strcmp(operation, "rev") == 0) {
@@ -63,6 +40,45 @@ int Calculate(Stack *stack, char *operation) {
         Push(stack, sqrt(Pop(stack)));
     } else if (strcmp(operation, "exp") == 0) {
         Push(stack, exp(Pop(stack)));
+    } else if (stack->len < 2)
+        return -1;
+    else if (strcmp(operation, "+") == 0) {
+        double n1 = Pop(stack);
+        double n2 = Pop(stack);
+        Push(stack, n1 + n2);
+    } else if (strcmp(operation, "-") == 0) {
+        double n1 = Pop(stack);
+        double n2 = Pop(stack);
+        Push(stack, n2 - n1);
+    } else if (strcmp(operation, "*") == 0) {
+        double n1 = Pop(stack);
+        double n2 = Pop(stack);
+        Push(stack, n2 * n1);
+    } else if (strcmp(operation, "/") == 0) {
+        double n1 = Pop(stack);
+        double n2 = Pop(stack);
+        Push(stack, n2 / n1);
+    } else if (strcmp(operation, "^") == 0) {
+        double n1 = Pop(stack);
+        double n2 = Pop(stack);
+        Push(stack, pow(n2, n1));
+    } else if (strcmp(operation, "sr") == 0) {
+        int count = (int) Pop(stack);
+        double sum = 0;
+        for (int i = 0; i < count; ++i) {
+            double el = Pop(stack);
+            if (el != 0.0 / 0.0) {
+                sum += el;
+            } else {
+                count = i;
+                break;
+            }
+        }
+        if (count > 0) {
+            Push(stack, sum / count);
+        } else {
+            Push(stack, 0);
+        }
     } else {
         return 1;
     }
